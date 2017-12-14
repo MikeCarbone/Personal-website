2d5e949258e79f784020030b17c96f437579d51f
mike-carbone <mfcbone@gmail.com>
2017-12-13 21:52:43 -0500

    changed imdb link

--- static/js/fancy.js
+++ static/js/fancy.js
@@ -1,20 +1,3 @@
-//$(document).ready(function(){
-//  $("a").on('click', function(event) {
-//
-//    if (this.hash !== "") {
-//      event.preventDefault();
-//      var hash = this.hash;
-//
-//      $('html, body').animate({
-//        scrollTop: $(hash).offset().top
-//      }, 800, function(){
-//   
-//        window.location.hash = hash;
-//      });
-//    }
-//  });
-//});
-
 $(document).ready(function($){
         var parPosition = [];
         $('.mover').each(function() {
